0b9cd25c215fa7d78c509556e8296c832e39ba2d
Jonathan Herman <lasercar7.gaming@gmail.com>
2016-12-03 19:52:13 -0800

    pretty tweaks

--- main.js
+++ main.js
@@ -2,8 +2,8 @@ const fs = require('fs')
 const yaml = require('js-yaml')
 
 try {
-  var doc = yaml.safeLoad(fs.readFileSync('userdata/9834b4d7-63b0-4c8f-b44e-cec7cca5840e.yml', 'utf8'));
-  console.log(doc);
+  let doc = yaml.safeLoad(fs.readFileSync('userdata/9834b4d7-63b0-4c8f-b44e-cec7cca5840e.yml', 'utf8'))
+  console.log(doc)
 } catch (e) {
-  console.log(e);
+  console.log(e)
 }
