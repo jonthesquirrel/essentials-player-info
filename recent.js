a1d985aa016c5c50077585aaf00ae00516cda386
Jonathan Herman <lasercar7.gaming@gmail.com>
2016-12-03 22:03:11 -0800

    cleanup unneeded code

--- recent.js
+++ recent.js
@@ -4,8 +4,7 @@ const yaml = require('js-yaml')
 let path = 'userdata'
 
 try {
-  // list files in userdata > load files and convert to json > sort, filter, and log player data
-  let result = fs.readdirSync(path)
+  fs.readdirSync(path)
     .map(filename => yaml.safeLoad(fs.readFileSync(`${path}/${filename}`, 'utf8')) )
     .map(player => ({name: player.lastAccountName, timestamp: player.timestamps.logout}) )
     .sort(player => player.timestamp)
