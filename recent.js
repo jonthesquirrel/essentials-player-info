186a939e62b90e6c66a5b92e5eb2563f7fa83016
Jonathan Herman <lasercar7.gaming@gmail.com>
2016-12-03 23:30:58 -0800

    use path argument or throw error

--- recent.js
+++ recent.js
@@ -1,10 +1,10 @@
 const fs = require('fs')
 const yaml = require('js-yaml')
 
-let path = 'userdata'
-
-try {
-  fs.readdirSync(path)
+if (typeof process.argv[2] === 'string') {
+  let path = process.argv[2]
+  try {
+    fs.readdirSync(path)
     .map(filename => yaml.safeLoad(fs.readFileSync(`${path}/${filename}`, 'utf8')) )
     .map(player => ({
       name: player.lastAccountName,
@@ -19,6 +19,9 @@ try {
     }))
     .map(player => `${player.name} [last session] ${player.lastLogin} - ${player.lastLogout}`)
     .forEach(player => console.log(player) )
-} catch (e) {
-  console.log(e)
+  } catch (e) {
+    console.log(e)
+  }
+} else {
+  throw {name: 'pathNotSpecified', message: 'please specify essentials userdata folder'}
 }
