1ef3151a6ead2e5f06e758ae5ba03a829ab2b0b5
Jonathan Herman <lasercar7.gaming@gmail.com>
2016-12-03 21:38:54 -0800

    list, convert, and print files using map

--- list-sort-recent.js
+++ list-sort-recent.js
@@ -4,9 +4,10 @@ const yaml = require('js-yaml')
 let path = 'userdata'
 
 try {
-  let doc = yaml.safeLoad(fs.readFileSync(`${path}/9834b4d7-63b0-4c8f-b44e-cec7cca5840e.yml`, 'utf8'))
   // list files in userdata > load files and convert to json > sort, filter, and log player data
-  console.log(doc)
+  let result = fs.readdirSync(path)
+    .map(filename => yaml.safeLoad(fs.readFileSync(`${path}/${filename}`, 'utf8')) )
+  console.log(result)
 } catch (e) {
   console.log(e)
 }
