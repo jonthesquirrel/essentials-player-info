5cc25cc04866aeb23162604ad04fc0ebb564cb92
Jonathan Herman <lasercar7.gaming@gmail.com>
2016-12-03 21:58:05 -0800

    condense map function returning object

--- recent.js
+++ recent.js
@@ -7,9 +7,7 @@ try {
   // list files in userdata > load files and convert to json > sort, filter, and log player data
   let result = fs.readdirSync(path)
     .map(filename => yaml.safeLoad(fs.readFileSync(`${path}/${filename}`, 'utf8')) )
-    .map(player => {
-      return {name: player.lastAccountName, timestamp: player.timestamps.logout}
-    })
+    .map(player => ({name: player.lastAccountName, timestamp: player.timestamps.logout}) )
     .sort(player => player.timestamp)
     .map(player => `${player.name} : ${new Date(player.timestamp)}`)
   console.log(result)
