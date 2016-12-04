df4d8e6e91b20fae346cfafc1bae5d3eeb712e29
Jonathan Herman <lasercar7.gaming@gmail.com>
2016-12-04 00:52:37 -0800

    add newlines to make clear what main code is

--- recent.js
+++ recent.js
@@ -4,6 +4,7 @@ const yaml = require('js-yaml')
 if (typeof process.argv[2] === 'string') {
   let path = process.argv[2]
   try {
+
     fs.readdirSync(path)
     .map(filename => yaml.safeLoad(fs.readFileSync(`${path}/${filename}`, 'utf8')) )
     .map(player => ({
@@ -19,6 +20,7 @@ if (typeof process.argv[2] === 'string') {
     }))
     .map(player => `${player.name} [last session] ${player.lastLogin} - ${player.lastLogout}`)
     .forEach(player => console.log(player) )
+
   } catch (e) {
     console.log(e)
   }
