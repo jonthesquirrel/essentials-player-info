fe6131e72a0f9cb1a7f1cd014567addcb246db4d
Jonathan Herman <lasercar7.gaming@gmail.com>
2016-12-03 21:22:53 -0800

    comment abstract plan

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ const yaml = require('js-yaml')
 
 try {
   let doc = yaml.safeLoad(fs.readFileSync('userdata/9834b4d7-63b0-4c8f-b44e-cec7cca5840e.yml', 'utf8'))
+  // list files in userdata > load files and convert to json > sort, filter, and log player data
   console.log(doc)
 } catch (e) {
   console.log(e)
