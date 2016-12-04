3f8fee87b7b501da545755515730a82862a2bee1
Jonathan Herman <lasercar7.gaming@gmail.com>
2016-12-03 21:27:57 -0800

    move path into variable

--- main.js
+++ main.js
@@ -1,8 +1,10 @@
 const fs = require('fs')
 const yaml = require('js-yaml')
 
+let path = 'userdata'
+
 try {
-  let doc = yaml.safeLoad(fs.readFileSync('userdata/9834b4d7-63b0-4c8f-b44e-cec7cca5840e.yml', 'utf8'))
+  let doc = yaml.safeLoad(fs.readFileSync(`${path}/9834b4d7-63b0-4c8f-b44e-cec7cca5840e.yml`, 'utf8'))
   // list files in userdata > load files and convert to json > sort, filter, and log player data
   console.log(doc)
 } catch (e) {
